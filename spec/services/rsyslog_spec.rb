a5a33fcc0b0c3b7eb3bf02eaaca3699521a8baa5
Miguel Negron <manegron@redborder.com>
2023-11-24 14:46:27 +0000

    Initial version

---
+++ spec/services/rsyslog_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+set :os, :family => 'redhat', :release => '9', :arch => 'x86_64'
+
+describe package('rsyslog') do
+  it { should be_installed }
+end
+
+describe service('rsyslog') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
