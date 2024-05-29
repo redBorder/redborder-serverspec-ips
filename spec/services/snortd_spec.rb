21a54f7dc47ff2d869bd473c98ebb4f89d134c70
Luis Blanco <ljblanco@redborder.com>
2024-05-29 11:20:12 +0100

    check snort is up and running

---
+++ spec/services/snortd_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'json'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+service = 'snortd'
+packages = %w[
+  snort
+]
+
+describe "Checking packages for #{service}..." do
+  packages.each do |package|
+    describe package(package) do
+      it 'is expected to be installed' do
+        expect(package(package).installed?).to be true
+      end
+    end
+  end
+end
+
+describe "Checking service_status service for #{service}..." do
+  describe service(service) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  # describe port(port) do
+  #   it { should be_listening }
+  # end
+end
