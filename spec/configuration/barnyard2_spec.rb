e547610ca63b7d8ed96d12bde651bb0286985eea
Luis Blanco <ljblanco@redborder.com>
2024-08-16 11:37:15 +0000

    just skip if first segment not configured

--- spec/configuration/barnyard2_spec.rb
+++ spec/configuration/barnyard2_spec.rb
@@ -6,15 +6,16 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 instances_group = %w[0] # This is the default name of the first instances group
 
 describe 'Barnyard2 config' do
-  instances_group.each do |group|
-    file_path = "/etc/snort/#{group}/barnyard2.conf"
-    describe file(file_path) do
-      if File.exist?(file_path)
+  file_path = '/etc/snort/0/barnyard2.conf'
+  if File.exist?(file_path)
+    skip("File #{file_path} does not exist, but is expected to exist on running state")
+  else
+    instances_group.each do |group|
+      file_path = "/etc/snort/#{group}/barnyard2.conf"
+      describe file(file_path) do
         it { should exist }
         it { should be_file }
         it { should be_readable }
-      else
-        skip("File #{file_path} does not exist, but is expected to exist on running state")
       end
     end
   end
